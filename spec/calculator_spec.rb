49cda7ac7ec305b3d311ccf159ca22aad2648d76
Prajakta Bande <prajaktabande1711@gmail.com>
2024-10-22 12:04:22 -0700

    Added positive test case for sum of 2 numbers

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe Calculator do
     it 'returns 0 for an empty string' do
       expect(Calculator.add("")).to eq(0)
     end
+
+    it 'returns the sum of two numbers' do
+      expect(Calculator.add("1,2")).to eq(3)
+    end
   end
 end
