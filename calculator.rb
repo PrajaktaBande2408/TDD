f92eb6380e08c205fb3345eb425435867a4f1ce0
Prajakta Bande <prajaktabande1711@gmail.com>
2024-10-22 12:05:26 -0700

    Updated add method to support addition operation of two numbers passed in string with ,

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,15 @@
 class Calculator
   def self.add(numbers)
     return 0 if numbers.empty?
+
+    num_array = numbers.split(",").map(&:to_i)
+
+    sum = 0
+    num_array.each do |n|
+      sum += n
+    end
+  
+    return sum
+
   end
 end
