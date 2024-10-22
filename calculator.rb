0c17fae81e7f5b733ab8baa8fe2291965353328b
Prajakta Bande <prajaktabande1711@gmail.com>
2024-10-22 12:50:53 -0700

    added list of regex for multiple delimiter in input string

--- calculator.rb
+++ calculator.rb
@@ -2,8 +2,16 @@ class Calculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
+    #Here we can extend multiple delimiters list in below Regexp
+    delimiter_regex_list = /[\n,]/
+
+    #Replace input strings all delimiter with ,
+    numbers = numbers.gsub(delimiter_regex_list, ",")
+
+    #Splitting the input string with delimiter like , or /n and storing it to array
     num_array = numbers.split(",").map(&:to_i)
 
+    #adding each element of array as sum
     sum = 0
     num_array.each do |n|
       sum += n
@@ -12,4 +20,5 @@ class Calculator
     return sum
 
   end
+
 end
